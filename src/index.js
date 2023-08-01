c331b39560f89cfc519fb16e6d9162dadbe53173
indrajeetambadekar <indrajeet.ambadekar@gmail.com>
2023-08-01 18:55:18 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./assets/styles/index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { UiProvidor, ToastProvider } from "elysium-cloud-ui";
+import "elysium-cloud-ui/index.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <UiProvidor>
+        <App />
+      </UiProvidor>
+    </ToastProvider>
   </React.StrictMode>
 );
 
